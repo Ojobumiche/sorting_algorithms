c109e81ac41077f4bcc73c52690a8cbd73deaff1
Ojobumiche <bumiche@gmail.com>
2023-04-26 07:07:19 -0700

    heap_sorting_algorithms

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -42,3 +42,30 @@ void max_heapify(int *array, size_t size, size_t base, size_t root)
 		max_heapify(array, size, base, large);
 	}
 }
+
+/**
+ * heap_sort - a function that sorts an array of integers in ascending
+ * order using the heap sort algorithm.
+ * @array: array of integers.
+ * @size: size of the array.
+ *
+ * Description: implements the sift-down heap sort
+ * algorithm. prints the array after each swap.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int x;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (x = (size / 2) - 1; x >= 0; x--)
+		max_heapify(array, size, size, x);
+
+	for (x = size - 1; x > 0; x--)
+	{
+		swap_ints(array, array + x);
+		print_array(array, size);
+		max_heapify(array, size, x, 0);
+	}
+}
